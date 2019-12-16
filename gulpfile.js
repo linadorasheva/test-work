a0d7bd85c4f1fa62933885ee7414559a5626ff47
lina_dorasheva <30377767+linadorasheva@users.noreply.github.com>
2019-12-16 13:01:28 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
-// 'use strict';
+'use strict';
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
-var autoprefixer = require('gulp-autoprefixer');
 var server = require('browser-sync').create();
 
 gulp.task('serve', function(done) {
@@ -27,7 +25,6 @@ gulp.task('serve', function(done) {
 
 gulp.task('style', function(done) {
   return gulp.src('source/sass/style.scss')
-    // .pipe(plumber())
     .pipe(sass())
 
     .pipe(gulp.dest('source/css'))
